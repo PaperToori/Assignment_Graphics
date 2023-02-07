95e5815f84634644833ac810b95c849075a4f8a4
Karl Garratt <garratt.karl@elev.ga.ntig.se>
2023-02-07 14:59:37 +0100

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import javax.swing.*;
+import java.awt.Color;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        // MiniFrame mF = new MiniFrame();
+        new MiniFrame();
+
+
+
+        // JFrame right into Main
+        /*
+        JFrame f = new JFrame();
+
+        f.setTitle("Is Hackathon fun?");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setResizable(false);
+        f.setSize(690,690);
+        f.setVisible(true);
+
+        ImageIcon img = new ImageIcon("top text.jpg");
+        f.setIconImage(img.getImage());
+        f.getContentPane().setBackground(new Color(255,150,180));
+         */
     }
 }
--- src/MiniFrame.java
+++ src/MiniFrame.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class MiniFrame {
+import javax.swing.*;
+import java.awt.Color;
+
+public class MiniFrame extends JFrame {
+
+    MiniFrame(){
+//        JFrame f = new JFrame();
+
+        this.setTitle("Is Hackathon fun?");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setSize(690,690);
+        this.setVisible(true);
+
+        ImageIcon img = new ImageIcon("top text.jpg");
+        this.setIconImage(img.getImage());
+        this.getContentPane().setBackground(new Color(255,150,180));
+    }
+
 }
